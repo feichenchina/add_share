448151268833fb9d9e7a498a45a5bd1730b2dd2c
feichenchina <tiejipiaoliu@163.com>
2020-04-29 14:18:49 +0800

    更新脚本并添加新的测试脚本，测试脚本为获取当前目录

--- new1.bat
+++ new1.bat
@@ -3,10 +3,11 @@ REM 声明采用UTF-8编码
 chcp 65001
 echo                                      Git自动更新脚本
 echo ===================================================================================
+cd /d %~dp0
+
 echo.
 set /p change=请输入Git更新变动（message内容）:
 echo.
-cd "C:\\Users\\98113\\Desktop\\python知识网站分享"
 
 git add .
 git commit -m %change%
